e444c97221386082138a9d8a135a406709a67370
Julius Franklin <jjfranklin88@outlook.com>
2021-10-07 21:24:24 -0400

    commit with functions and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,59 +4,101 @@ const { execSync } = require('child_process');
 
 const fakeRequest = require('supertest');
 const app = require('../lib/app');
-const client = require('../lib/client');
+// const client = require('../lib/client');
 
 describe('app routes', () => {
   describe('routes', () => {
     let token;
-  
+
     beforeAll(async () => {
       execSync('npm run setup-db');
-  
-      await client.connect();
-      const signInData = await fakeRequest(app)
-        .post('/auth/signup')
-        .send({
-          email: 'jon@user.com',
-          password: '1234'
-        });
-      
-      token = signInData.body.token; // eslint-disable-line
-    }, 10000);
-  
-    afterAll(done => {
-      return client.end(done);
+
+      //   await client.connect();
+      //   const signInData = await fakeRequest(app)
+      //     .post('/auth/signup')
+      //     .send({
+      //       email: 'jon@user.com',
+      //       password: '1234'
+      //     });
+
+      //   token = signInData.body.token; // eslint-disable-line
+      // }, 10000);
+
+      // afterAll(done => {
+      //   return client.end(done);
     });
 
-    test('returns animals', async() => {
+    test('returns city data', async () => {
 
-      const expectation = [
-        {
-          'id': 1,
-          'name': 'bessie',
-          'cool_factor': 3,
-          'owner_id': 1
-        },
-        {
-          'id': 2,
-          'name': 'jumpy',
-          'cool_factor': 4,
-          'owner_id': 1
-        },
-        {
-          'id': 3,
-          'name': 'spot',
-          'cool_factor': 10,
-          'owner_id': 1
-        }
-      ];
+      const expectation = {
+        Name: expect.any(String),
+        latitude: expect.any(String),
+        longitude: expect.any(String)
+      };
 
       const data = await fakeRequest(app)
-        .get('/animals')
+        .get('/location?search=boston')
         .expect('Content-Type', /json/)
         .expect(200);
 
       expect(data.body).toEqual(expectation);
+    });
+    test('returns weather data', async () => {
+
+      const expectation = [{
+        forecast: expect.any(Number),
+        time: expect.any(String),
+      },
+      {
+        forecast: expect.any(Number),
+        time: expect.any(String),
+      },
+      {
+        forecast: expect.any(Number),
+        time: expect.any(String),
+      },
+      {
+        forecast: expect.any(Number),
+        time: expect.any(String),
+      },
+      {
+        forecast: expect.any(Number),
+        time: expect.any(String),
+      },
+      {
+        forecast: expect.any(Number),
+        time: expect.any(String),
+      },
+      {
+        forecast: expect.any(Number),
+        time: expect.any(String),
+      }
+      ];
+
+      const data = await fakeRequest(app)
+        .get('/weather?latitude=42.3602534&longitude=-71.0582912')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+    test('returns review data', async () => {
+
+      const expectation = [{
+        name: expect.any(String),
+        image: expect.any(String),
+        rating: expect.any(Number),
+        price: expect.any(String),
+        website: expect.any(String)
+      }];
+
+
+      const data = await fakeRequest(app)
+        .get('/reviews?latitude=42.3602534&longitude=-71.0582912')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expect.arrayContaining(expectation));
     });
   });
 });
---
+++ data/utils.js
@@ -0,0 +1,38 @@
+function grabLocationData(body) {
+    return {
+        Name: body.display_name,
+        latitude: body.lat,
+        longitude: body.lon
+    };
+}
+
+
+
+function grabWeatherData(weatherData) {
+
+    const forecast = weatherData.slice(0, 7);
+    const weatherArray = forecast.map(weather => {
+        return {
+            forecast: weather.temp,
+            time: weather.datetime,
+
+        };
+    });
+    return weatherArray;
+}
+
+function grabReviewData(reviewData) {
+    return reviewData.map(review => {
+        return {
+            name: review.name,
+            image: review.image_url,
+            rating: review.rating,
+            price: review.price,
+            website: review.url
+        };
+    }
+    )
+}
+module.exports = {
+    grabLocationData, grabWeatherData, grabReviewData
+};
